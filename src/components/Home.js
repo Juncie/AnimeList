9e4bc0ebc05fc6330feb0b0dccc792bad2ae79b8
Brandon <BrandonMitchell5365@gmail.com>
2021-07-06 09:54:03 -0700

    TopAnime

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
                 <div className="wrapper">    
                 
                 <input type='text' placeholder='Begin your search here...'></input>
-                <div id='box' className='TopAnime-box'>Top Anime</div>
-                <div id='box'className='Genres-box'>Genres</div>
-                <div id='box'className='Manga-box'>Manga</div>
-                <div id='box'className='Random-box'>???</div>
-                <footer style={{gridArea:"footer", position:'relative', top:'28vh'}} className='foot'>Follow Us:</footer>
+                <div id="box" className='topAnime-box'>Top Anime</div>
+                <div id="box" className='Genres-box'>Genres</div>
+                <div id="box" className='Manga-box'>Manga</div>
+                <div id="box" className='Random-box'>???</div>
+                <footer style={{gridArea:'footer', position:'relative', top: '28vh'}} className='foot'>Follow Us:</footer>
                 
                 </div>
         </div>
@@ -18,3 +18,16 @@ const Home = () => {
 };
 
 export default Home;
+
+const dailyFun = () => {
+    let mornings=['breakfast', 'work', 'sleep', 'cry']
+    let lies = []
+     for (let i = 0; i< mornings.length; i++) {
+     if(mornings[i].includes('s')){
+        lies.push(mornings[i])
+     }
+     return lies
+     }
+     }
+    
+   dailyFun()
--- src/components/TopAnime.js
+++ src/components/TopAnime.js
@@ -5,25 +5,43 @@ import { Link } from 'react-router-dom';
 function TopAnime() {
 
 const [anime, setAnime] = useState([])
-
+useEffect(() =>{
+    GetTopAnime();
+    console.log('this is working')
+}, [])
 
 const GetTopAnime = async () => {
     const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
         .then(res => res.json());
-        setAnime(temp.top.slice(0, 5));
-} 
+        setAnime(temp.top);
+    } 
+    
+    const showTop = () =>{
+       return anime.map((eachAnime, i) => {
+            console.log(eachAnime.title);
+            return (
+                <Link to={eachAnime.url}>
+                <div key={i}>
+                   
+                        <img src={eachAnime.image_url} />
+                        <h2>{eachAnime.title}</h2>
+                         
+                        </div>
+                   
+                    </Link>
+                )
+            })
+            
+        }
+        console.log(anime)
 
 
-useEffect(() =>{
-    GetTopAnime();
-}, [])
-
-console.log(anime)
 
  return (
     
-        <div>
+        <div className="show-box">
             
+        {showTop()}
         </div>
     );
 }
